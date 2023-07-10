7c20a3f7e174941cb745abc47ad6f4c80d046aef
vijaykadpewad <vijaykadpewad01@gmail.com>
2023-07-10 17:02:31 +0530

    First commit

---
+++ Hello.java
@@ -0,0 +1,35 @@
+class Hello
+{
+public static void main(String args[])
+{
+System.out.println("Hello");
+
+System.out.println("Welcome");
+System.out.println("Name : vijay");
+System.out.println("Mob :820880253");
+System.out.println("Add : Tq.umri Dis : Nanded");
+System.out.println("Adhar Number : 266408066035");
+System.out.println("Poly : Gramin Poly Nanded");
+System.out.println("School : nutan");
+System.out.println("Pin Code : 431807");
+System.out.println("Class :softtech");
+System.out.println("tata");
+System.out.println("Hcl");
+System.out.println("infosys");
+System.out.println("mahindra");
+System.out.println("kadpewad");
+System.out.println("7385838731");
+System.out.println("8308705706");
+System.out.println("7058333431");
+System.out.println("9423657271");
+System.out.println("9028992201");
+System.out.println("rameshwaram");
+System.out.println("1234");
+System.out.println("95678");
+System.out.println("847");
+
+
+
+
+}
+}
